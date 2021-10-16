a7e5b1a6974cdb41ed87b40034aeb181a83df41b
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2021-10-16 19:42:07 +0100

    random data generator in js

---
+++ generator.js
@@ -0,0 +1,14 @@
+let names = ['John', 'Sally', 'Nick', 'dog', 'Local gossip'];
+let action = ['digging the soil', 'knitting', 'talking nonsense', 'asking for trouble', 'creating drama'];
+let place = ['in the warehouse', 'in the woods', 'outside the town', 'in the secret spot', 'in the factory', 'on the farm'];
+
+function newPrompt(){
+
+let  randomName = names[Math.floor(Math.random() * names.length)];
+let  randomAction = action[Math.floor(Math.random() * names.length)]; 
+let  randomPlace = place[Math.floor(Math.random() * names.length)];
+
+console.log(`${randomName} ${randomAction} ${randomPlace}.`);
+}
+
+newPrompt();
